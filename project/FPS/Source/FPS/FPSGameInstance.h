55bb83a16b3f86b8771864f8558a38176aeee812
wangaiguo <wangaiguo@fh-game.com>
2019-07-23 18:36:59 +0800

    add support for SpatialOS

---
+++ project/FPS/Source/FPS/FPSGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EngineClasses/SpatialGameInstance.h"
+#include "FPSGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_API UFPSGameInstance : public USpatialGameInstance
+{
+	GENERATED_BODY()
+	
+};
